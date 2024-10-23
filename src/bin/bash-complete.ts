cd647c1a473efb7fbc06fc0c3678422f2ed42dc1
Mauro Erta <mauro.erta@sap.com>
2024-10-23 19:05:01 +0200

    fix: postintall

--- src/bin/bash-complete.ts
+++ src/bin/bash-complete.ts
@@ -7,3 +7,14 @@ if (process.env["COMP_LINE"]?.endsWith(" ")) {
   inputs.push("");
 }
 await proposeCompletions(app, inputs, buildContext(process));
+try {
+  for (const { completion } of await proposeCompletions(
+    app,
+    inputs,
+    buildContext(process)
+  )) {
+    process.stdout.write(`${completion}\n`);
+  }
+} catch {
+  // ignore
+}
